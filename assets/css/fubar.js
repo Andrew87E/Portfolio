431e7c2b72412e4d54c3125a6232272b70b1d513
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-06-20 12:33:20 -0500

    added resposive css

--- assets/css/fubar.js
+++ assets/css/fubar.js
@@ -1,9 +1,19 @@
 
-function myHambagurger() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
+// function hambagurger() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function hambagurger() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
   } else {
-    x.style.display = "block";
+    x.className = "navbar";
   }
 }
