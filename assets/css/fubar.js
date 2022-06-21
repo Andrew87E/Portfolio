411777df2c50be70569bf70094f5f04ccb4bcc2c
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-06-21 15:25:17 -0500

    fixed links

--- assets/css/fubar.js
+++ assets/css/fubar.js
@@ -1,13 +1,3 @@
-
-// function hambagurger() {
-//   var x = document.getElementById("myLinks");
-//   if (x.style.display === "block") {
-//     x.style.display = "none";
-//   } else {
-//     x.style.display = "block";
-//   }
-// }
-
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function hambagurger() {
   var x = document.getElementById("myNavbar");
